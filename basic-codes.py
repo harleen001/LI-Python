6db9c05a08fea531677145912904b5c8ebb0e0cc
harleen001 <harleen.johal31@gmail.com>
2026-01-12 09:40:24 +0530

    list,set,dictionary

--- basic-codes.py
+++ basic-codes.py
@@ -4,5 +4,22 @@ y=x*6
 # print( _ +2) # _ represent output of previous operation
 nums=[10,20,30]
 names=["kartik","harleen"]
-mix=[nums,names]
+mix=[nums,names]  #list of lists
 print(mix)
+
+tup=(10,20,30)
+print(tup[0])
+# tup[0]=46   TUPLE DOESNOT SUPPORT ITEM ASSIGNMENT AS IT IS IMMUTABLE
+print(tup[0])
+
+
+#SET
+print("Set in python")
+abc={10,20,32,32,12,43,53,54,64,10}
+print(abc)
+
+#Dictionary using 2 lists
+key=[1,2,3]
+names=["Kartik","Harleen","Imanpal"]
+dict1=dict(zip(key,names))
+print(dict1)
