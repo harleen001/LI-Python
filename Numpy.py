bc3c9fed4a5dbc7f3324b3f86f39e467d0843ade
harleen001 <harleen.johal31@gmail.com>
2026-01-15 22:31:24 +0530

    linspace

--- Numpy.py
+++ Numpy.py
@@ -8,3 +8,13 @@ print(arr)
 
 A=linspace(0,15,20) #breaking into 20 parts
 print(A)
+
+B=arange(1,10,2)
+print(B)       
+# zeros(5) and ones(5)
+
+
+arr1=array([1,2,3,4,5])
+arr2=array([1,2,3,4,5])
+arr3=arr1+arr2
+print(arr3) #Vectorized operation in an array
