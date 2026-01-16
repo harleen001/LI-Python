ff534d9820568e9390fafdbdcc1a1e680d73edae
harleen001 <harleen.johal31@gmail.com>
2026-01-16 20:10:30 +0530

    matrix

---
+++ Matrix.py
@@ -0,0 +1,9 @@
+from numpy import *
+arr1= array([
+        [1,2,3],
+        [4,5,6]
+])
+print(arr1)
+print(arr1.dtype) #datatype
+print(arr1.ndim) #dimension
+print(arr1.shape) #no of rows and cols
