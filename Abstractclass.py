3a9898379200bcf7dc75bfa913fc65110dea6311
harleen001 <harleen.johal31@gmail.com>
2026-01-21 18:42:17 +0530

    abc

---
+++ Abstractclass.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+class Computer(ABC):
+    @abstractmethod
+    def process(self):
+        pass
+
+com=Computer()
+com.process()
--- Using-cls.py
+++ Using-cls.py
@@ -6,7 +6,9 @@ class student:
         self.name=name
         self.rollno=rollno
     
+    @classmethod     #decorator to specify it as class method, now no need to pass class as an argument, 
+    #use @staticmethod for no self/cls just simple static function execution
     def displayname(cls):
         print("My name is",cls.stu)
 
-student.displayname(student)
+student.displayname()
