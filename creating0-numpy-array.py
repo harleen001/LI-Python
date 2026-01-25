4cb213512351c273486cf2b30eaba74b9974f0bd
harleen001 <harleen.johal31@gmail.com>
2026-01-25 17:48:01 +0530

    array-creation

---
+++ creating0-numpy-array.py
@@ -0,0 +1,17 @@
+import numpy as np
+arr_1d=np.array([1,2,3,4,5])
+print(arr_1d)
+
+arr_2d=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(arr_2d)
+
+
+arr_zeros=np.zeros((3,2),)
+print(arr_zeros)
+
+arr_ones=np.ones((3,2),)
+print(arr_ones)
+
+
+arr_identity=np.eye(3,dtype=int)
+print(arr_identity)
