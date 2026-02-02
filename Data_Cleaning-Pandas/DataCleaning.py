9cc40de59fe51f07563b925a61b70d06b3de2c74
harleen001 <harleen.johal31@gmail.com>
2026-02-02 12:31:00 +0530

    Renaming-data-cleaning-values

--- Data_Cleaning-Pandas/DataCleaning.py
+++ Data_Cleaning-Pandas/DataCleaning.py
@@ -11,6 +11,28 @@ df.columns.tolist()   #get the columns as a list
 
 df.columns.view()     #to only view the column names
 
-df.columns.summary()   #for a summary of column names/DEPRECEATED BY NOW
+#df.columns.summary()   #for a summary of column names/DEPRECEATED BY NOW
 
 df.columns.to_series()   #CONVERTING COLUMN NAMES TO A SERIES FOR USE IN PANDAS FURTHER
+
+df.columns.to_frame()   #CONVERTING COLUMNS TO DATA FRAMES DIRECTLY
+
+#df.columns.contains('First Name')   #to check if column name contains 'First Name' or not
+
+df.columns.duplicated() #returns false as most of the values are unique and not duplicate
+
+
+dir(df.columns.str)  # shows attributes and methods in string
+
+
+#---------------------WORKING ON DATA------------------------------
+df1=df.rename(columns={'Age':'Date of Birth'})   #RENAMING COLUMN  to a different copy
+#print(df1)
+df.rename(columns={'Age':'Date of Birth'},inplace=True)   #on same place
+print(df)
+
+
+df.columns.values[7] = 'Email Address'   #renaming using selected columns
+print(df)
+
+print(df.columns)
