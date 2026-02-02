0c0808d2cacfef38fe45ac0406440c52e67b9d39
harleen001 <harleen.johal31@gmail.com>
2026-02-02 12:24:24 +0530

    data-cleaning

--- Data_Cleaning-Pandas/DataCleaning.py
+++ Data_Cleaning-Pandas/DataCleaning.py
@@ -3,4 +3,14 @@ df = pd.read_csv("raw_dataset.csv")
 
 df.head()  #output of first 5 values 
 df.columns #columns print
-print(dir(df.columns)) #directories of that colums
+dir(df.columns) #directories of that colums
+
+df.columns.values    #printing columns as an array
+
+df.columns.tolist()   #get the columns as a list
+
+df.columns.view()     #to only view the column names
+
+df.columns.summary()   #for a summary of column names/DEPRECEATED BY NOW
+
+df.columns.to_series()   #CONVERTING COLUMN NAMES TO A SERIES FOR USE IN PANDAS FURTHER
