64b3b7c466fab53cf65003b98ff764a6b32a4946
harleen001 <harleen.johal31@gmail.com>
2026-02-10 13:32:36 +0530

    facewash-bae

---
+++ matplotlib/facewash-facecream-bar.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv("company_sales_data.csv")
+print(df.head())
+x=df['month_number']
+
+plt.bar(x,df['facecream'], label='Facecream Sales Data',width=0.1,color='blue')
+plt.bar(x,df['facewash'], label='Facewash Sales Data',width=0.5,color='orange')
+
+
+plt.xlabel("Month Number")
+plt.ylabel("Number of units sold")
+plt.legend(loc='upper left') 
+plt.grid()
+plt.show()  #printed data,
