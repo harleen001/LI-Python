89c9c8118afc77c3f7e4ebc4bddd01b2ece1f3ad
harleen001 <harleen.johal31@gmail.com>
2026-02-10 18:51:34 +0530

    line-plot

--- matplotlib/Intellipath-basics.py
+++ matplotlib/Intellipath-basics.py
@@ -3,6 +3,18 @@ import pandas as pd
 
 x=[1,2,3]
 y=[4,5,6]
-plt.plot(x,y)
-plt.grid()  #to get grid
-plt.show()   #simple plot
+#plt.plot(x,y)
+#plt.grid()  #to get grid
+#plt.show()   #simple plot
+
+
+#PyPlot API 
+#Univariate (single data) - Numerical
+Data={
+    "Salary": [25000,30000,70000,45000,32000,61000,29000,38000,31000,45000]
+}
+df=pd.DataFrame(Data)
+
+#LINE PLOT
+plt.plot(df['Salary'])
+plt.show()
