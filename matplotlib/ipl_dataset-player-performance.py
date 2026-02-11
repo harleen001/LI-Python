42f503777ee2cc6462a32c3999ec4572473fe823
harleen001 <harleen.johal31@gmail.com>
2026-02-11 13:33:42 +0530

    winner-team

--- matplotlib/ipl_dataset-player-performance.py
+++ matplotlib/ipl_dataset-player-performance.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df=pd.read_csv("batsman.csv")
+df = pd.read_csv("batsman.csv")
 batsman_runs = df.groupby('batter')['score'].sum().reset_index()
 batsman_runs = batsman_runs.sort_values(by='score', ascending=False)
 
-batsmanvalue=batsman_runs[(batsman_runs['score']!=0) & (batsman_runs['score']>=500)]
-x=batsmanvalue['batter']
-y=batsmanvalue['score']
+batsmanvalue = batsman_runs[(batsman_runs['score'] != 0) & (batsman_runs['score'] >= 500)]
+x = batsmanvalue['batter']
+y = batsmanvalue['score']
+
 print(batsmanvalue.head())
 
-plt.pie(y,labels=x)
-plt.show() 
+plt.pie(y,labels=x,autopct=lambda pct: f'{int(round(pct * sum(y) / 100.0))}')
+plt.title('Top Batsman Scores')
+plt.show()
--- matplotlib/ipl_dataset-winnerteam.py
+++ matplotlib/ipl_dataset-winnerteam.py
@@ -1,11 +1,18 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df_player=pd.read_csv("ipl_data.csv")
+df_player=pd.read_csv("batsman.csv")
 df_match=pd.read_csv("match.csv")
 
 df_merged = pd.merge(df_player, df_match, left_on='match_no', right_on='match_number', how='left')
 
+
+df_merged['batting_team'] = df_merged.apply(lambda row: row['team1'] if row['inningno'] == 1 else row['team2'], axis=1)
+
+
 team_runs = df_merged.groupby('batting_team')['score'].sum().reset_index()
 team_runs = team_runs.sort_values(by='score', ascending=False)
-print(team_runs.head())
+top10winners=team_runs.head(5)
+
+plt.bar(top10winners['batting_team'],top10winners['score'])
+plt.show()
