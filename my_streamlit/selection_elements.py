770bb7d658ab0ead582e389b55ceb563e18b1752
harleen001 <harleen.johal31@gmail.com>
2026-02-25 08:51:55 +0530

    selection-elements

--- my_streamlit/selection_elements.py
+++ my_streamlit/selection_elements.py
@@ -13,3 +13,60 @@ sentiment_mapping = ["one", "two", "three", "four", "five"]
 selected = st.feedback("stars")
 if selected is not None:
     st.markdown(f"You selected {sentiment_mapping[selected]} star(s).")
+
+#multiselect
+options = st.multiselect(
+    "What are your favorite colors?",
+    ["Green", "Yellow", "Red", "Blue"],
+    default=["Yellow", "Red"],
+)
+
+st.write("You selected:", options)
+
+#pill options
+
+options = ["North", "East", "South", "West"]
+selection = st.pills("Directions", options, selection_mode="multi")
+st.markdown(f"Your selected options: {selection}.")
+
+#radio button
+genre = st.radio(
+    "What's your favorite movie genre",
+    [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"],
+    captions=[
+        "Laugh out loud.",
+        "Get the popcorn.",
+        "Never stop learning.",
+    ],
+)
+
+if genre == ":rainbow[Comedy]":
+    st.write("You selected comedy.")
+else:
+    st.write("You didn't select comedy.")
+
+
+#same line segment controls
+options = ["North", "East", "South", "West"]
+selection = st.segmented_control(
+    "Directions", options, selection_mode="multi"
+)
+st.markdown(f"Your selected options: {selection}.")
+
+
+
+#slider
+start_color, end_color = st.select_slider(
+    "Select a range of color wavelength",
+    options=[
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+    ],
+    value=("red", "blue"),
+)
+st.write("You selected wavelengths between", start_color, "and", end_color)
