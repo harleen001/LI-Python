5acf9e06e6fdb2fdd3b1c501d8c76d1b6fec6452
harleen001 <harleen.johal31@gmail.com>
2026-02-26 12:09:00 +0530

    plotly-streamlit

--- matplotlib/product-visualization.py
+++ matplotlib/product-visualization.py
@@ -16,6 +16,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import plotly.express as px
+
 d={'Pid':pd.Series([1,2,3,4,5,6,7]),
    'Pname':pd.Series(['Rin','Lux','Pepsi','Eraser','Pen','Pencil','Register']),
    'Prate':pd.Series([85,90,35,30,96,20,150])}
@@ -25,3 +27,7 @@ plt.plot(df['Pname'],df['Prate'])
 for x,y in zip(df['Pname'],df['Prate']):
     plt.text(x,y+5,y)   # x and y location and coordinate to show location
 st.pyplot(plt)
+
+
+fig = px.line(df, x=df["Pname"], y=df["Prate"], title='Product Information',text=df["Prate"])
+st.plotly_chart(fig)
