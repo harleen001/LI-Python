9a7de3d983fef3c0a889f2b12b6302836d96eb5c
harleen001 <harleen.johal31@gmail.com>
2026-02-27 11:13:59 +0530

    page-switch

--- my_streamlit/pages/page_2.py
+++ my_streamlit/pages/page_2.py
@@ -1,8 +1,3 @@
 import streamlit as st
 
-def page_2():
-    st.title("Page 2")
-    st.page_link("page_1.py", query_params={"utm_source": "page_2"})
-
-pg = st.navigation([page_2, "page_1.py"])
-pg.run()
+st.title("Page 2")
