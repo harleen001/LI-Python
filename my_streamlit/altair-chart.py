8c02c893185dd8912c8e1ad7956bb76fc12911d3
harleen001 <harleen.johal31@gmail.com>
2026-03-06 11:22:26 +0530

    altair-chart

---
+++ my_streamlit/altair-chart.py
@@ -0,0 +1,14 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(rng(0).standard_normal((60, 3)), columns=["a", "b", "c"])
+
+chart = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.altair_chart(chart)
