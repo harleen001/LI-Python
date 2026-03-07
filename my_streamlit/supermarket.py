adbe47d4cfd5717fa6083a3f90b65a64ba9c25ae
harleen001 <harleen.johal31@gmail.com>
2026-03-07 12:58:29 +0530

    plotly-streamlit

--- my_streamlit/supermarket.py
+++ my_streamlit/supermarket.py
@@ -1,47 +1,50 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(layout="wide")
 
 df = pd.read_csv("superstore.csv")
+st.title("Supermarket Dashboard")
 
-col1, col2 = st.columns([2, 1]) 
+col1, col2 = st.columns([3, 1]) 
 
 with col2:
     category = st.selectbox("Select Category", options=df['Category'].unique())
-    
     sub_df = df[df['Category'] == category]
-    unique_subs = sub_df['Sub-Category'].unique()
     
     selected_subs = []
     st.write("Select Sub-Categories:")
-    for sub in unique_subs:
-        if st.checkbox(sub, key=sub, value=False):
+    for sub in sub_df['Sub-Category'].unique():
+        if st.checkbox(sub, key=sub, value=True):
             selected_subs.append(sub)
 
-filtered_df = df[(df['Category'] == category) & (df['Sub-Category'].isin(selected_subs))]
+filtered_df = df[(df['Category'] == category) & (df['Sub-Category'].isin(selected_subs))].head(10).copy()
+
 
 with col1:
     col1A, col1B = st.columns(2)
     
     with col1A:
         st.subheader("Sales")
-        st.bar_chart(filtered_df['Sales'])
+        fig_sales = px.area(filtered_df, x="Product Name", y="Sales", text=filtered_df['Sales'],color_discrete_sequence=['#0000FF'])
+        fig_sales.update_traces(texttemplate='%{text:.2s}', textposition='top center')
+        st.plotly_chart(fig_sales, use_container_width=True)
+        
         
         st.subheader("Profit")
-        st.bar_chart(filtered_df['Profit'])
+        fig_profit = px.line(filtered_df, x="Product Name", y="Profit", text=filtered_df['Profit'],color_discrete_sequence=['#008000'])
+        fig_profit.update_traces(texttemplate='%{text:.2s}', textposition='top center')
+        st.plotly_chart(fig_profit, use_container_width=True)
     
     with col1B:
         st.subheader("Discount")
-        st.bar_chart(filtered_df['Discount'])
+        fig_disc = px.bar(filtered_df, x="Product Name", y="Discount", text=filtered_df['Discount'],color_discrete_sequence=['#FFFF00'])
+        fig_disc.update_traces(texttemplate='%{text:.2f}', textposition='outside')
+        st.plotly_chart(fig_disc, use_container_width=True)
 
 st.markdown("""
     <style>
-    .block-container {
-        padding-top: 5rem;
-        padding-bottom: 0rem;
-        padding-left: 1rem;
-        padding-right: 1rem;
-    }
+    .block-container { padding-top: 2rem; }
     </style>
     """, unsafe_allow_html=True)
