186bccf250d5ffa60751bc4d72ded4ab21b737b3
harleen001 <harleen.johal31@gmail.com>
2026-03-13 12:45:49 +0530

    pandas-categorical-variables-fetched

--- machine-learning/relative-cpu-performance-linear-regression.py
+++ machine-learning/relative-cpu-performance-linear-regression.py
@@ -10,3 +10,16 @@ filename = "machine-test.csv"
 df = pd.read_csv(filename, header = None)
 
 print("Shape of dataframe df: {}".format(df.shape))
+col_names = ['Vendor Name','Model Name', 'MYCT', 'MMIN', 'MMAX', 'CACH','CHMIN', 'CHMAX', 'PRP', 'ERP' ]
+df.columns = col_names
+
+#finding categorical variables and searching them
+categorical = [col for col in df.columns if df[col].dtype=='O']
+print('There are {} categorical variables'.format(len(categorical)))
+
+print(categorical)
+
+
+df['Vendor Name'].value_counts()
+print('Number of unique Model Names: ', len(df['Model Name'].unique()))
+print('Number  of instances of models: ', len(df))
