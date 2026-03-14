db534cdb478e1c60ffc08f8d47dc4231c751c053
harleen001 <harleen.johal31@gmail.com>
2026-03-14 13:07:56 +0530

    plotly-plot

---
+++ machine-learning/LinearRegression-Pepsi-plotly.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from CustomLinearRegression import MyLinearRegression
+from CustomR import get_accuracy
+import numpy as np
+import plotly.express as plt
+
+df = pd.read_csv("Pepsi.csv")
+model = MyLinearRegression()
+
+x = df['Year'].values 
+y = df['Actual_Price'].values
+model.fit(x, y)
+y_pred = model.predict(x)
+
+df['Predicted_Price'] = y_pred
+
+print(df.head(10))
+
+new_x=np.array([2026,2027,2028,2029,2030])
+new_prices=model.predict(new_x)
+
+future_df = pd.DataFrame({'Year': new_x,'Actual_Price': np.nan,'Predicted_Price': new_prices})
+
+
+
+df = pd.concat([df, future_df], ignore_index=True)
+print(df.head(15))
+fig = plt.scatter(df, x='Year', y='Actual_Price', title=f'Pepsi Price Forecast',labels={'Actual_Price': 'Price', 'Year': 'Year'})
+fig.add_traces(plt.line(df, x='Year', y='Predicted_Price').data[0])
+fig.update_traces(line_color='green', line_dash='dash')
+fig.update_layout(template="plotly_white")
+fig.show()
+
+
+accuracy = get_accuracy(x, y, model)
+
+print(f"Model Accuracy (R²): {accuracy:.4f}")
+if accuracy > 0.90:
+    print("The model is highly accurate!")
+else:
+    print("The model might need more data or a different approach.")
