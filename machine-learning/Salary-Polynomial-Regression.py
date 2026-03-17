0f99d58c7f4955d48afda1691efe0d1b26550f77
harleen001 <harleen.johal31@gmail.com>
2026-03-17 12:34:50 +0530

    minor

--- machine-learning/Salary-Polynomial-Regression.py
+++ machine-learning/Salary-Polynomial-Regression.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 
 
 dataset = pd.read_csv('salary.csv')
-X = dataset.iloc[:, 1:2].values
+X = dataset.iloc[:, 1:2].values     #divide kita x and y de vich
 y = dataset.iloc[:,2].values
 
 
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
-lin_reg.fit(X,y)
+lin_reg.fit(X,y)    #fitting model for linear regression   TRAINNN
 
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
-lin_reg_2 = LinearRegression()
+lin_reg_2 = LinearRegression()    # built in polynomail regression
 lin_reg_2.fit(X_poly, y)
 
 
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.plot(X, lin_reg.predict(X), color = 'blue')    #predicting model     TEST LINEAR
 plt.title('Linear Regression')
 plt.xlabel('Level')
 plt.ylabel('Salary')
@@ -29,7 +29,7 @@ plt.show()
 
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
-plt.title('Polynomial Regression')
+plt.title('Polynomial Regression')   #predicting model   TEST POLYNOMIAL
 plt.xlabel('Level')
 plt.ylabel('Salary')
 plt.show()
