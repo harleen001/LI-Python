92c6f61f57ace28ffba812e806789413028539e4
harleen001 <harleen.johal31@gmail.com>
2026-03-22 21:09:08 +0530

    DIFFERCING

---
+++ machine-learning/Feature-eng-differencing.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data = pd.DataFrame({
+    'Date': pd.date_range(start='2023-01-01', periods=5, freq='D'),
+    'value': [10, 12, 15, 18, 20]
+})
+data['lag_1'] = data['value'].shift(1)
+data['lag_2'] = data['value'].shift(2)
+print(data)
+data['diff_1'] = data['value'].diff(periods=1)
+print(data)
