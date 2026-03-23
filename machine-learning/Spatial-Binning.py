c2089426b0dd1c8a91c8d092f472cb6586429711
harleen001 <harleen.johal31@gmail.com>
2026-03-23 10:53:28 +0530

    spatial binning

---
+++ machine-learning/Spatial-Binning.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+data = pd.DataFrame({'Latitude': [40.7728, 34.0552, 41.8741],
+                     'Longitude': [-74.0060, -118.2437, -87.6298]})
+
+data['lat_bin'] = (data['Latitude'] // 0.1) * 0.1
+data['lon_bin'] = (data['Longitude'] // 0.1) * 0.1
+print(data)
