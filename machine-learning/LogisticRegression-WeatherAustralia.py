4ff71780236e7d75ab18a0f8e8f58685536af917
harleen001 <harleen.johal31@gmail.com>
2026-03-24 16:49:10 +0530

    weather australia

---
+++ machine-learning/LogisticRegression-WeatherAustralia.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+data = 'weatherAUS.csv'
+df = pd.read_csv(data)
+col_names = df.columns
+# find categorical variables
+
+categorical = [var for var in df.columns if df[var].dtype=='O']
+
+print('There are {} categorical variables\n'.format(len(categorical)))
+
+print('The categorical variables are :', categorical)
