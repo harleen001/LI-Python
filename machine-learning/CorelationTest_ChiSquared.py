37972ee245e6f4d7d2e14376896b1b71935d3f28
harleen001 <harleen.johal31@gmail.com>
2026-03-28 11:39:15 +0530

    stationary test

---
+++ machine-learning/CorelationTest_ChiSquared.py
@@ -0,0 +1,9 @@
+# Example of the Chi-Squared Test
+from scipy.stats import chi2_contingency
+table = [[10, 20, 30],[6,  9,  17]]
+stat, p, dof, expected = chi2_contingency(table)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
