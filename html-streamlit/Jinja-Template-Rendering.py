733c4f48e22d1d2aec1fe6dedabd4a86385addda
harleen001 <harleen.johal31@gmail.com>
2026-03-30 12:30:11 +0530

    Jinja Template

--- html-streamlit/Jinja-Template-Rendering.py
+++ html-streamlit/Jinja-Template-Rendering.py
@@ -2,12 +2,16 @@ from jinja2 import Template
 import streamlit.components.v1 as components
 
 def render_table(data):
-               with open("templates/table.html") as table:
-                   template = Template(table.read())
-                   return template.render(
-                       data=data
-               )
+    with open("templates/table.html") as table_file:
+        template = Template(table_file.read())
+        return template.render(data=data)
 
 
-table = render_table(data=data) # type: ignore
-components.html(table)
+data = [
+    {"column1": "Row 1, Col 1", "column2": "Row 1, Col 2"},
+    {"column1": "Row 2, Col 1", "column2": "Row 2, Col 2"}
+]
+
+# Now you can call the function because 'data' exists
+table_html = render_table(data=data) 
+components.html(table_html)
