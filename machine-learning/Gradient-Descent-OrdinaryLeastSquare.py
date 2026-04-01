10471dc90d76539e2f7660ba5a2a04593b97444d
harleen001 <harleen.johal31@gmail.com>
2026-04-01 13:09:33 +0530

    Multiple OLS Gradient Descent

--- machine-learning/Gradient-Descent-OrdinaryLeastSquare.py
+++ machine-learning/Gradient-Descent-OrdinaryLeastSquare.py
@@ -15,8 +15,36 @@ reg.fit(X,y)
 # and let's assume the starting value for intercept b = 0
 y_pred = ((78.35 * X) + 100).reshape(4)
 
+# plt.scatter(X,y)
+# plt.plot(X,reg.predict(X),color='red',label='OLS')
+# plt.plot(X,y_pred,color='#00a65a',label='b = 0')
+# plt.legend()
+# plt.show()
+
+import numpy as np
+# next iteration
+m = 78.35
+b = 100
+
+loss_slope = -2 * np.sum(y - m*X.ravel() - b)
+loss_slope
+# Lets take learning rate = 0.1
+lr = 0.1
+
+step_size = loss_slope*lr
+step_size
+# Calculating the new intercept
+b = b - step_size
+b
+
+y_pred1 = ((78.35 * X) + b).reshape(4)
+
 plt.scatter(X,y)
 plt.plot(X,reg.predict(X),color='red',label='OLS')
-plt.plot(X,y_pred,color='#00a65a',label='b = 0')
+plt.plot(X,y_pred1,color='#00a65a',label='b = {}'.format(b))
+plt.plot(X,y_pred,color='#A3E4D7',label='b = 0')
 plt.legend()
 plt.show()
+
+
+#similar way do multiple iterations OLS
