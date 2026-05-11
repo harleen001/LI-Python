c93f79fcf5cba7639f8c06988cad108c00b4ee4e
harleen001 <harleen.johal31@gmail.com>
2026-05-11 12:11:49 +0530

    natural language processing

---
+++ machine-learning/natural-language-processing/basic.py
@@ -0,0 +1,15 @@
+import nltk
+#nltk.download('stopwords')
+from nltk.corpus import stopwords
+import pandas as pd
+import numpy as np
+from warnings import filterwarnings
+filterwarnings('ignore')
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
+
+#creating strings
+text = "language"
+print(text)
+print(3*text)
