f3c9e17d6836625daaec99631d50752a859486df
harleen001 <harleen.johal31@gmail.com>
2026-05-16 14:19:20 +0530

    Integer Encoding

---
+++ machine-learning/IntegerEncoding.py
@@ -0,0 +1,23 @@
+vocab = {}
+word_encoding = 1
+def one_hot_encoding(text):
+  global word_encoding
+
+  words = text.lower().split(" ")
+  encoding = []
+
+  for word in words:
+    if word in vocab:
+      code = vocab[word]
+      encoding.append(code)
+    else:
+      vocab[word] = word_encoding
+      encoding.append(word_encoding)
+      word_encoding += 1
+
+  return encoding
+
+text = "this is a test to see if this test will work is is test a a"
+encoding = one_hot_encoding(text)
+print(encoding)
+print(vocab)
